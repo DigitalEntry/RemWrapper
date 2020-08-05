1dae7617fa2aad4ffa14e8f9746b356f43751d31
Aidan C <36613705+DigitalEntry@users.noreply.github.com>
2020-08-05 17:45:27 -0400

    Add files via upload

---
+++ Class.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RemAPIWrapper
+{
+	public class RemAPI
+	{
+		private string Token;
+
+		public RemAPI(string Token)
+		{
+			this.Token = Token;
+		}
+
+		public async Task<string> Rem()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Add("Token", Token);
+				HttpResponseMessage response = await client.GetAsync("https://api.rembot.cc/images/sfw/rem");
+				HttpContent content = response.Content;
+
+				string result = await content.ReadAsStringAsync();
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					return result;
+				}
+				else
+				{
+					throw new HttpListenerException(int.Parse(response.StatusCode.ToString()), $"Server returned an error. {response.StatusCode} {result}");
+				}
+			}
+		}
+
+		public async Task<string> LewdRem()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Add("Token", Token);
+				HttpResponseMessage response = await client.GetAsync("https://api.rembot.cc/images/nsfw/rem");
+				HttpContent content = response.Content;
+
+				string result = await content.ReadAsStringAsync();
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					return result;
+				}
+				else
+				{
+					throw new HttpListenerException(int.Parse(response.StatusCode.ToString()), $"Server returned an error. {response.StatusCode} {result}");
+				}
+			}
+		}
+
+		public async Task<string> Thighs()
+		{
+			using (HttpClient client = new HttpClient())
+			{
+				client.DefaultRequestHeaders.Add("Token", Token);
+				HttpResponseMessage response = await client.GetAsync("https://api.rembot.cc/images/nsfw/thighs");
+				HttpContent content = response.Content;
+
+				string result = await content.ReadAsStringAsync();
+				if (response.StatusCode == HttpStatusCode.OK)
+				{
+					return result;
+				}
+				else
+				{
+					throw new HttpListenerException(int.Parse(response.StatusCode.ToString()), $"Server returned an error. {response.StatusCode} {result}");
+				}
+			}
+		}
+	}
+}
